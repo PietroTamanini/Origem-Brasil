3f4b4c8b9b567904d78cf3197073bca6bc84b316
Felipe Afonso de Souza <felipeafonsodes@gmail.com>
2026-09-16 16:31:33 -0300

    caminho certo do banco

--- conexao.php
+++ conexao.php
@@ -6,7 +6,7 @@ if (session_status() === PHP_SESSION_NONE) {
 $host    = "localhost";
 $usuario = "root";
 $senha   = "";
-$banco   = "ORIGEM";
+$banco   = "br";
 
 $conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
